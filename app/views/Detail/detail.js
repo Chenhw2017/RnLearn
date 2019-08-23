bf5e5b987ebf4d1ebaf9689cc1cff658237698f7
chenhw <chenhw2019@gmail.com>
2019-08-23 14:24:14 +0800

    初始化代码

---
+++ app/views/Detail/detail.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {View} from 'react-native';
+// import {
+
+// } from 'react-navigation';
+
+class Detail extends React.Component {
+  render() {
+    <Fragment>
+      <View>Detail</View>
+    </Fragment>;
+  }
+}
+
+export default Detail;
